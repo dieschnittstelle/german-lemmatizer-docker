ed13f86fa89c69714c6c4901cdf78fcb3a643c42
jk <joern.kreutel@beuth-hochschule.de>
2019-11-11 22:15:18 +0100

    change function name

---
+++ run.sh
@@ -0,0 +1,13 @@
+g# source for tiger corpus: https://www.ims.uni-stuttgart.de/forschung/ressourcen-manuell-zu-bearbeiten/korpora/TIGERCorpus/download/start.html
+
+# build
+docker build -t dieschnittstelle/german_lemmatiser .
+
+# stop&remove existing containers
+docker stop flfddr_german_lemmatiser
+docker rm flfddr_german_lemmatiser
+
+# run
+docker run -itd -p 7090:8082 --name flfddr_german_lemmatiser dieschnittstelle/german_lemmatiser
+
+
